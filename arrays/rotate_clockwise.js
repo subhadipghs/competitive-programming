7b8c060a130d29e255258c9945cf092288ccb664
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-03-21 12:07:13 +0530

    rotate array done

--- arrays/rotate_clockwise.js
+++ arrays/rotate_clockwise.js
@@ -8,9 +8,28 @@ function rotateClockWise(arr = [], n = 0) {
   return arr;
 }
 
+function rotate(a, n) {
+  let low = 0, high = n - 1;
+  let curr = a[low];
+  do {
+    let next = a[(low + 1) % n];
+    a[(low + 1) % n] = curr;
+    low++;
+    curr = next;
+  } while(low <= high);
+  return a;
+}
+  /*
+function swap(arr, i, j) {
+  arr[i] == arr[i] ^ arr[j];
+  arr[i] == arr[i] ^ arr[j];
+arr[j] == arr[i] ^ arr[j];
+}
+*/
 
 const tcs = [
   {
+    name: 'should rotate the array by 1 element',
     input: [[1, 2, 3, 4, 5], 5],
     exp: [5, 1, 2, 3, 4]
   }
@@ -18,9 +37,12 @@ const tcs = [
 
 function test(fn) {
   tcs.forEach(tc => {
+    console.log("Running: " + tc.name);
     const res = fn(...tc.input);
     deepEqual(res, tc.exp);
+    console.log("✔ Passed: " + tc.name);
   })
 }
 
+test(rotate)
 test(rotateClockWise)
