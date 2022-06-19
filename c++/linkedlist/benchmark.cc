6be7f3219e3232a8fb09823b85bee72d46a5f126
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-06-19 10:07:04 +0530

    added: check cycle

---
+++ c++/linkedlist/benchmark.cc
@@ -0,0 +1,27 @@
+#include <chrono>
+
+/* Only needed for the sake of this example. */
+#include <iostream>
+#include <thread>
+
+void
+benchmark (void *(*ops) (void))
+{
+  using std::chrono::duration;
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
+
+  auto t1 = high_resolution_clock::now ();
+  ops ();
+  auto t2 = high_resolution_clock::now ();
+
+  /* Getting number of milliseconds as an integer. */
+  auto ms_int = duration_cast<milliseconds> (t2 - t1);
+
+  /* Getting number of milliseconds as a double. */
+  duration<double, std::milli> ms_double = t2 - t1;
+
+  std::cout << ms_int.count () << "ms\n";
+  std::cout << ms_double.count () << "ms\n";
+}
---
+++ c++/linkedlist/check_cycle.cc
@@ -0,0 +1,59 @@
+
+
+#include "linked_list_api.h"
+#include <bits/stdc++.h>
+
+// check whether the linked list has cycle
+bool
+has_cycle (ListNode *head)
+{
+  ListNode *iter = head;
+  while (iter != nullptr)
+    {
+      if (iter->val == 10e5)
+        return true;
+      else
+        iter->val = 10e5;
+      iter = iter->next;
+    }
+  return false;
+}
+
+// two pointer solution to the problem is this
+bool
+has_cycle_two_pointer (ListNode *head)
+{
+  ListNode *slow = head;
+  ListNode *fast = head;
+
+  while (fast != nullptr && fast->next != nullptr)
+    {
+      if (slow->val == fast->val)
+        return true;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+  return false;
+}
+
+void
+should_check_for_cycle (bool (*fn) (ListNode *list))
+{
+
+  ListNode *head = new ListNode (1);
+  ListNode *conn = new ListNode (2);
+  head->next = conn;
+  head->next->next = new ListNode (4);
+  head->next->next->next = new ListNode (7);
+  head->next->next->next->next = conn;
+  bool r = fn (head);
+  assert (r == true);
+  std::cout << "should_check_for_cycle passed 🏁" << std::endl;
+}
+
+int
+main (int argc, char **argv)
+{
+  should_check_for_cycle (has_cycle);
+  return 0;
+}
