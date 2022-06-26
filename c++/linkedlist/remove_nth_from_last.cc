5fb8781d11b2de08142efda86a149dbfd5a1f1a1
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-06-26 18:31:02 +0530

    solved removed nth node from last

---
+++ c++/linkedlist/remove_nth_from_last.cc
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+#include "linked_list_api.cc"
+
+using namespace std;
+
+ListNode* remove_nth_node_from_last(ListNode* head, int n)
+{
+  int count = 0;
+  ListNode* iter = head;
+
+  if (!head) {
+    return head; 
+  }
+
+  while(iter) {
+    count++;
+    iter = iter->next;
+  }
+
+  int node_index = count - n;
+  ListNode *prev = nullptr, *curr = head;
+
+  for (int i = 0; i < node_index; i++) {
+    prev = curr;
+    curr = curr->next;
+  }
+
+
+  if (!curr) {
+    return nullptr;
+  } else if (!prev) {
+    head = curr->next;
+  } else {
+    prev->next = curr->next;
+  }
+  delete curr;
+  return head;
+}
+
+
+int main(int argc, char** argv)
+{
+
+  vector<int> v = {1};
+  int n = 1;
+  ListNode* head = make_list(v);
+  ListNode* res = remove_nth_node_from_last(head, n);
+  debug_list(res);
+  return 0;
+}
